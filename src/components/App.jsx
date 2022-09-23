06f190c958452e75164e4e92760252390643af99
massuhcodes <massuhcodes@gmail.com>
2022-09-22 23:28:26 -0400

    Further continued with project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,62 @@
+import "/src/styles/App.css";
+import { useEffect, useState } from "react";
 import { data } from "../utils/data.js";
+import Notification from "./Notification.jsx";
 
 export default function App() {
-    console.log(data);
-    return;
+    const [users, setUsers] = useState(data);
+    const [count, setCount] = useState(
+        users.filter((user) => user.unread).length
+    );
+
+    useEffect(
+        () => setCount(users.filter((user) => user.unread).length),
+        [users]
+    );
+
+    function toggleAllNotificationReads() {
+        setUsers(
+            users.map((user) => ({
+                ...user,
+                // must toggle truth
+                unread: !(count > 0),
+            }))
+        );
+    }
+
+    function toggleSingleNotificationRead(id) {
+        setUsers(
+            users.map((user) => {
+                return user.id === id
+                    ? { ...user, unread: !user.unread }
+                    : user;
+            })
+        );
+    }
+
+    const notifications = users.map((user) => (
+        <Notification
+            key={user.id}
+            id={user.id}
+            user={user}
+            toggleSingleNotificationRead={toggleSingleNotificationRead}
+        />
+    ));
+    return (
+        <div className="app">
+            <header>
+                <div className="title--count">
+                    <h2 className="title">Notifications</h2>
+                    <h2 className="count">{count}</h2>
+                </div>
+                <span
+                    onClick={toggleAllNotificationReads}
+                    className="toggler-of-all-notifications"
+                >
+                    {count > 0 ? "Make all as read" : "Make all as unread"}
+                </span>
+            </header>
+            <main>{notifications}</main>
+        </div>
+    );
 }
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,3 +1,42 @@
-export default function Notification() {
-    return;
+import "/src/styles/Notification.css";
+
+export default function Notification(props) {
+    const {
+        profilePicture,
+        name,
+        type,
+        event,
+        post,
+        group,
+        message,
+        picture,
+        unread,
+        duration,
+    } = props.user;
+
+    return (
+        <section
+            className={`section-notification ${unread ? "unread" : "read"}`}
+            onClick={() => props.toggleSingleNotificationRead(props.id)}
+        >
+            <div className="outer-div">
+                <img className="profile-picture" src={profilePicture} />
+                <div className="inner-div">
+                    <p>
+                        <span className="name">{name}</span>{" "}
+                        <span className="event">{event}</span>{" "}
+                        {type === "1" ? (
+                            <span className="post">{post}</span>
+                        ) : (
+                            <span className="group">{group}</span>
+                        )}{" "}
+                        {unread && <span className="dot">•</span>}
+                    </p>
+                    <time>{duration}</time>
+                    {message && <p className="message">{message}</p>}
+                </div>
+            </div>
+            {picture && <img className="notification-picture" src={picture} />}
+        </section>
+    );
 }
