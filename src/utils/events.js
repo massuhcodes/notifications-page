8b24aba569cecee4899f71fde00309092805ec3a
massuhcodes <massuhcodes@gmail.com>
2022-09-26 18:15:36 -0400

    Implemented events object in order to determine notification type

---
+++ src/utils/events.js
@@ -0,0 +1,12 @@
+// Every notification has a specific event
+
+const events = {
+    1: "reacted to your recent post",
+    2: "followed you",
+    3: "has joined your group",
+    4: "sent you a private message",
+    5: "commented on your picture",
+    6: "left the group",
+};
+
+export { events };
