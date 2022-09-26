92bfb1e96b05368cb2cef2bb8f1516532f251668
massuhcodes <massuhcodes@gmail.com>
2022-09-26 15:54:53 -0400

    Implemented backup data in the event any fetching fails.

---
+++ src/utils/backup-data.js
@@ -0,0 +1,71 @@
+// Intended as backup data in the event that any fetching fails
+
+import angelaGray from "/src/assets/avatar-angela-gray.webp";
+import annaKim from "/src/assets/avatar-anna-kim.webp";
+import jacobThompson from "/src/assets/avatar-jacob-thompson.webp";
+import kimberlySmith from "/src/assets/avatar-kimberly-smith.webp";
+import markWebber from "/src/assets/avatar-mark-webber.webp";
+import nathanPeterson from "/src/assets/avatar-nathan-peterson.webp";
+import rizkyHasanuddin from "/src/assets/avatar-rizky-hasanuddin.webp";
+import chess from "/src/assets/image-chess.webp";
+
+// backup for user data
+const backupUsers = [
+    {
+        avatar: angelaGray,
+        first_name: "Angela",
+        last_name: "Gray",
+        alt: "A picture of Angela Gray",
+    },
+    {
+        avatar: annaKim,
+        first_name: "Anna",
+        last_name: "Kim",
+        alt: "A picture of Anna Kim",
+    },
+    {
+        avatar: jacobThompson,
+        first_name: "Jacob",
+        last_name: "Thompson",
+        alt: "A picture of Jacob Thompson",
+    },
+    {
+        avatar: kimberlySmith,
+        first_name: "Kimberly",
+        last_name: "Smith",
+        alt: "A picture of Kimberly Smith",
+    },
+    {
+        avatar: markWebber,
+        first_name: "Mark",
+        last_name: "Webber",
+        alt: "A picture of Mark Webber",
+    },
+    {
+        avatar: nathanPeterson,
+        first_name: "Nathan",
+        last_name: "Peterson",
+        alt: "A picture of Nathan Peterson",
+    },
+    {
+        avatar: rizkyHasanuddin,
+        first_name: "Rizky",
+        last_name: "Hasanuddin",
+        alt: "A picture of Rizky Hasanuddin",
+    },
+];
+
+// backup for picture being commented on
+const backupPhoto = chess;
+
+// backup for message data
+const backupMessage = "Hey, when are we gonna get coffee?";
+
+// backup for post data
+const backupPost =
+    "After the Eiffel Tower was built, one person was killed during the installation of the lifts. No one was killed during the actual construction of the tower.";
+
+//backup for group data
+const backupGroup = "Chess";
+
+export { backupUsers, backupPhoto, backupMessage, backupPost, backupGroup };
