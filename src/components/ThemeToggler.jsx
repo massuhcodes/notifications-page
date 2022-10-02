c09da0183462c828596c81dbfd2e4fdfe266dd85
massuhcodes <massuhcodes@gmail.com>
2022-10-01 23:42:02 -0400

    Adding Theme Toggler

---
+++ src/components/ThemeToggler.jsx
@@ -0,0 +1,26 @@
+import "/src/styles/ThemeToggler.css";
+import night from "/src/assets/night.png";
+import sunny from "/src/assets/sunny.png";
+import { useEffect, useState } from "react";
+
+export const THEME_KEY = "theme";
+
+export default function ThemeToggler() {
+    const persistedTheme = localStorage.getItem(THEME_KEY);
+    const [theme, setTheme] = useState(persistedTheme ?? "light");
+
+    useEffect(() => {
+        document.body.className = theme === "light" ? "light" : "dark"
+        localStorage.setItem(THEME_KEY, theme);
+    }, [theme]);
+
+    function toggleTheme() {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    }
+
+    return (
+        <div className={"toggler"} onClick={toggleTheme}>
+            <img id="toggler-image" src={theme === "light" ? night : sunny} />
+        </div>
+    );
+}
